352df635b8820cedabc513c2695b8a5e8f70c3f9
20deanoklein-sketch <20deanoklein@gmail.com>
2025-12-06 00:02:27 -0600

    Day 2: added homepage and tournaments page

---
+++ app/tournaments/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function TournamentsPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Tournaments</h1>
+      <p>This will eventually show the Oklahoma bass tournament schedule.</p>
+    </main>
+  );
+}
